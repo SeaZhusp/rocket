4a3ca9277157ce95e53401384cc47d95c8121845
zhusp <370172879@qq.com>
2023-02-14 23:00:03 +0800

    add httprunner , version=2.2.4

---
+++ httprunner/api.py
@@ -0,0 +1,313 @@
+# encoding: utf-8
+
+import os
+import unittest
+
+from httprunner import (exceptions, loader, logger, parser, report, runner,
+                        utils, validator)
+
+
+class HttpRunner(object):
+
+    def __init__(self, failfast=False, save_tests=False, report_template=None, report_dir=None,
+        log_level="INFO", log_file=None):
+        """ initialize HttpRunner.
+
+        Args:
+            failfast (bool): stop the test run on the first error or failure.
+            save_tests (bool): save loaded/parsed tests to JSON file.
+            report_template (str): report template file path, template should be in Jinja2 format.
+            report_dir (str): html report save directory.
+            log_level (str): logging level.
+            log_file (str): log file path.
+
+        """
+        self.exception_stage = "initialize HttpRunner()"
+        kwargs = {
+            "failfast": failfast,
+            "resultclass": report.HtmlTestResult
+        }
+        self.unittest_runner = unittest.TextTestRunner(**kwargs)
+        self.test_loader = unittest.TestLoader()
+        self.save_tests = save_tests
+        self.report_template = report_template
+        self.report_dir = report_dir
+        self._summary = None
+        if log_file:
+            logger.setup_logger(log_level, log_file)
+
+    def _add_tests(self, testcases):
+        """ initialize testcase with Runner() and add to test suite.
+
+        Args:
+            testcases (list): testcases list.
+
+        Returns:
+            unittest.TestSuite()
+
+        """
+        def _add_test(test_runner, test_dict):
+            """ add test to testcase.
+            """
+            def test(self):
+                try:
+                    test_runner.run_test(test_dict)
+                except exceptions.MyBaseFailure as ex:
+                    self.fail(str(ex))
+                finally:
+                    self.meta_datas = test_runner.meta_datas
+
+            if "config" in test_dict:
+                # run nested testcase
+                test.__doc__ = test_dict["config"].get("name")
+                variables = test_dict["config"].get("variables", {})
+            else:
+                # run api test
+                test.__doc__ = test_dict.get("name")
+                variables = test_dict.get("variables", {})
+
+            if isinstance(test.__doc__, parser.LazyString):
+                try:
+                    parsed_variables = parser.parse_variables_mapping(variables)
+                    test.__doc__ = parser.parse_lazy_data(
+                        test.__doc__, parsed_variables
+                    )
+                except exceptions.VariableNotFound:
+                    test.__doc__ = str(test.__doc__)
+
+            return test
+
+        test_suite = unittest.TestSuite()
+        for testcase in testcases:
+            config = testcase.get("config", {})
+            test_runner = runner.Runner(config)
+            TestSequense = type('TestSequense', (unittest.TestCase,), {})
+
+            tests = testcase.get("teststeps", [])
+            for index, test_dict in enumerate(tests):
+                times = test_dict.get("times", 1)
+                try:
+                    times = int(times)
+                except ValueError:
+                    raise exceptions.ParamsError(
+                        "times should be digit, given: {}".format(times))
+
+                for times_index in range(times):
+                    # suppose one testcase should not have more than 9999 steps,
+                    # and one step should not run more than 999 times.
+                    test_method_name = 'test_{:04}_{:03}'.format(index, times_index)
+                    test_method = _add_test(test_runner, test_dict)
+                    setattr(TestSequense, test_method_name, test_method)
+
+            loaded_testcase = self.test_loader.loadTestsFromTestCase(TestSequense)
+            setattr(loaded_testcase, "config", config)
+            setattr(loaded_testcase, "teststeps", tests)
+            setattr(loaded_testcase, "runner", test_runner)
+            test_suite.addTest(loaded_testcase)
+
+        return test_suite
+
+    def _run_suite(self, test_suite):
+        """ run tests in test_suite
+
+        Args:
+            test_suite: unittest.TestSuite()
+
+        Returns:
+            list: tests_results
+
+        """
+        tests_results = []
+
+        for testcase in test_suite:
+            testcase_name = testcase.config.get("name")
+            logger.log_info("Start to run testcase: {}".format(testcase_name))
+
+            result = self.unittest_runner.run(testcase)
+            tests_results.append((testcase, result))
+
+        return tests_results
+
+    def _aggregate(self, tests_results):
+        """ aggregate results
+
+        Args:
+            tests_results (list): list of (testcase, result)
+
+        """
+        summary = {
+            "success": True,
+            "stat": {
+                "testcases": {
+                    "total": len(tests_results),
+                    "success": 0,
+                    "fail": 0
+                },
+                "teststeps": {}
+            },
+            "time": {},
+            "platform": report.get_platform(),
+            "details": []
+        }
+
+        for tests_result in tests_results:
+            testcase, result = tests_result
+            testcase_summary = report.get_summary(result)
+
+            if testcase_summary["success"]:
+                summary["stat"]["testcases"]["success"] += 1
+            else:
+                summary["stat"]["testcases"]["fail"] += 1
+
+            summary["success"] &= testcase_summary["success"]
+            testcase_summary["name"] = testcase.config.get("name")
+            testcase_summary["in_out"] = utils.get_testcase_io(testcase)
+
+            report.aggregate_stat(summary["stat"]["teststeps"], testcase_summary["stat"])
+            report.aggregate_stat(summary["time"], testcase_summary["time"])
+
+            summary["details"].append(testcase_summary)
+
+        return summary
+
+    def run_tests(self, tests_mapping):
+        """ run testcase/testsuite data
+        """
+        project_mapping = tests_mapping.get("project_mapping", {})
+        if self.save_tests:
+            utils.dump_logs(tests_mapping, project_mapping, "loaded")
+
+        # parse tests
+        self.exception_stage = "parse tests"
+        parsed_testcases = parser.parse_tests(tests_mapping)
+
+        if self.save_tests:
+            utils.dump_logs(parsed_testcases, project_mapping, "parsed")
+
+        # add tests to test suite
+        self.exception_stage = "add tests to test suite"
+        test_suite = self._add_tests(parsed_testcases)
+
+        # run test suite
+        self.exception_stage = "run test suite"
+        results = self._run_suite(test_suite)
+
+        # aggregate results
+        self.exception_stage = "aggregate results"
+        self._summary = self._aggregate(results)
+
+        # generate html report
+        self.exception_stage = "generate html report"
+        report.stringify_summary(self._summary)
+
+        if self.save_tests:
+            utils.dump_logs(self._summary, project_mapping, "summary")
+
+        report_path = report.render_html_report(
+            self._summary,
+            self.report_template,
+            self.report_dir
+        )
+
+        return report_path
+
+    def get_vars_out(self):
+        """ get variables and output
+        Returns:
+            list: list of variables and output.
+                if tests are parameterized, list items are corresponded to parameters.
+
+                [
+                    {
+                        "in": {
+                            "user1": "leo"
+                        },
+                        "out": {
+                            "out1": "out_value_1"
+                        }
+                    },
+                    {...}
+                ]
+
+            None: returns None if tests not started or finished or corrupted.
+
+        """
+        if not self._summary:
+            return None
+
+        return [
+            summary["in_out"]
+            for summary in self._summary["details"]
+        ]
+
+    def run_path(self, path, dot_env_path=None, mapping=None):
+        """ run testcase/testsuite file or folder.
+
+        Args:
+            path (str): testcase/testsuite file/foler path.
+            dot_env_path (str): specified .env file path.
+            mapping (dict): if mapping is specified, it will override variables in config block.
+
+        Returns:
+            instance: HttpRunner() instance
+
+        """
+        # load tests
+        self.exception_stage = "load tests"
+        tests_mapping = loader.load_tests(path, dot_env_path)
+        tests_mapping["project_mapping"]["test_path"] = path
+
+        if mapping:
+            tests_mapping["project_mapping"]["variables"] = mapping
+
+        return self.run_tests(tests_mapping)
+
+    def run(self, path_or_tests, dot_env_path=None, mapping=None):
+        """ main interface.
+
+        Args:
+            path_or_tests:
+                str: testcase/testsuite file/foler path
+                dict: valid testcase/testsuite data
+
+        """
+        if validator.is_testcase_path(path_or_tests):
+            return self.run_path(path_or_tests, dot_env_path, mapping)
+        elif validator.is_testcases(path_or_tests):
+            return self.run_tests(path_or_tests)
+        else:
+            raise exceptions.ParamsError("Invalid testcase path or testcases: {}".format(path_or_tests))
+
+    @property
+    def summary(self):
+        """ get test reuslt summary.
+        """
+        return self._summary
+
+
+def prepare_locust_tests(path):
+    """ prepare locust testcases
+
+    Args:
+        path (str): testcase file path.
+
+    Returns:
+        list: locust tests data
+
+            [
+                testcase1_dict,
+                testcase2_dict
+            ]
+
+    """
+    tests_mapping = loader.load_tests(path)
+    testcases = parser.parse_tests(tests_mapping)
+
+    locust_tests = []
+
+    for testcase in testcases:
+        testcase_weight = testcase.get("config", {}).pop("weight", 1)
+        for _ in range(testcase_weight):
+            locust_tests.append(testcase)
+
+    return locust_tests
---
+++ httprunner/runner.py
@@ -0,0 +1,402 @@
+# encoding: utf-8
+
+from unittest.case import SkipTest
+
+from httprunner import exceptions, logger, response, utils
+from httprunner.client import HttpSession
+from httprunner.context import SessionContext
+
+
+class Runner(object):
+    """ Running testcases.
+
+    Examples:
+        >>> tests_mapping = {
+                "project_mapping": {
+                    "functions": {}
+                },
+                "testcases": [
+                    {
+                        "config": {
+                            "name": "XXXX",
+                            "base_url": "http://127.0.0.1",
+                            "verify": False
+                        },
+                        "teststeps": [
+                            {
+                                "name": "test description",
+                                "variables": [],        # optional
+                                "request": {
+                                    "url": "http://127.0.0.1:5000/api/users/1000",
+                                    "method": "GET"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+
+        >>> testcases = parser.parse_tests(tests_mapping)
+        >>> parsed_testcase = testcases[0]
+
+        >>> test_runner = runner.Runner(parsed_testcase["config"])
+        >>> test_runner.run_test(parsed_testcase["teststeps"][0])
+
+    """
+
+    def __init__(self, config, http_client_session=None):
+        """ run testcase or testsuite.
+
+        Args:
+            config (dict): testcase/testsuite config dict
+
+                {
+                    "name": "ABC",
+                    "variables": {},
+                    "setup_hooks", [],
+                    "teardown_hooks", []
+                }
+
+            http_client_session (instance): requests.Session(), or locust.client.Session() instance.
+
+        """
+        self.verify = config.get("verify", True)
+        self.export = config.get("export") or config.get("output", [])
+        self.validation_results = []
+        config_variables = config.get("variables", {})
+
+        # testcase setup hooks
+        testcase_setup_hooks = config.get("setup_hooks", [])
+        # testcase teardown hooks
+        self.testcase_teardown_hooks = config.get("teardown_hooks", [])
+
+        self.http_client_session = http_client_session or HttpSession()
+        self.session_context = SessionContext(config_variables)
+
+        if testcase_setup_hooks:
+            self.do_hook_actions(testcase_setup_hooks, "setup")
+
+    def __del__(self):
+        if self.testcase_teardown_hooks:
+            self.do_hook_actions(self.testcase_teardown_hooks, "teardown")
+
+    def __clear_test_data(self):
+        """ clear request and response data
+        """
+        if not isinstance(self.http_client_session, HttpSession):
+            return
+
+        self.validation_results = []
+        self.http_client_session.init_meta_data()
+
+    def __get_test_data(self):
+        """ get request/response data and validate results
+        """
+        if not isinstance(self.http_client_session, HttpSession):
+            return
+
+        meta_data = self.http_client_session.meta_data
+        meta_data["validators"] = self.validation_results
+        return meta_data
+
+    def _handle_skip_feature(self, test_dict):
+        """ handle skip feature for test
+            - skip: skip current test unconditionally
+            - skipIf: skip current test if condition is true
+            - skipUnless: skip current test unless condition is true
+
+        Args:
+            test_dict (dict): test info
+
+        Raises:
+            SkipTest: skip test
+
+        """
+        # TODO: move skip to initialize
+        skip_reason = None
+
+        if "skip" in test_dict:
+            skip_reason = test_dict["skip"]
+
+        elif "skipIf" in test_dict:
+            skip_if_condition = test_dict["skipIf"]
+            if self.session_context.eval_content(skip_if_condition):
+                skip_reason = "{} evaluate to True".format(skip_if_condition)
+
+        elif "skipUnless" in test_dict:
+            skip_unless_condition = test_dict["skipUnless"]
+            if not self.session_context.eval_content(skip_unless_condition):
+                skip_reason = "{} evaluate to False".format(skip_unless_condition)
+
+        if skip_reason:
+            raise SkipTest(skip_reason)
+
+    def do_hook_actions(self, actions, hook_type):
+        """ call hook actions.
+
+        Args:
+            actions (list): each action in actions list maybe in two format.
+
+                format1 (dict): assignment, the value returned by hook function will be assigned to variable.
+                    {"var": "${func()}"}
+                format2 (str): only call hook functions.
+                    ${func()}
+
+            hook_type (enum): setup/teardown
+
+        """
+        logger.log_debug("call {} hook actions.".format(hook_type))
+        for action in actions:
+
+            if isinstance(action, dict) and len(action) == 1:
+                # format 1
+                # {"var": "${func()}"}
+                var_name, hook_content = list(action.items())[0]
+                hook_content_eval = self.session_context.eval_content(hook_content)
+                logger.log_debug(
+                    "assignment with hook: {} = {} => {}".format(
+                        var_name, hook_content, hook_content_eval
+                    )
+                )
+                self.session_context.update_test_variables(
+                    var_name, hook_content_eval
+                )
+            else:
+                # format 2
+                logger.log_debug("call hook function: {}".format(action))
+                # TODO: check hook function if valid
+                self.session_context.eval_content(action)
+
+    def _run_test(self, test_dict):
+        """ run single teststep.
+
+        Args:
+            test_dict (dict): teststep info
+                {
+                    "name": "teststep description",
+                    "skip": "skip this test unconditionally",
+                    "times": 3,
+                    "variables": [],            # optional, override
+                    "request": {
+                        "url": "http://127.0.0.1:5000/api/users/1000",
+                        "method": "POST",
+                        "headers": {
+                            "Content-Type": "application/json",
+                            "authorization": "$authorization",
+                            "random": "$random"
+                        },
+                        "json": {"name": "user", "password": "123456"}
+                    },
+                    "extract": {},              # optional
+                    "validate": [],             # optional
+                    "setup_hooks": [],          # optional
+                    "teardown_hooks": []        # optional
+                }
+
+        Raises:
+            exceptions.ParamsError
+            exceptions.ValidationFailure
+            exceptions.ExtractFailure
+
+        """
+        # clear meta data first to ensure independence for each test
+        self.__clear_test_data()
+
+        # check skip
+        self._handle_skip_feature(test_dict)
+
+        # prepare
+        test_dict = utils.lower_test_dict_keys(test_dict)
+        test_variables = test_dict.get("variables", {})
+        self.session_context.init_test_variables(test_variables)
+
+        # teststep name
+        test_name = self.session_context.eval_content(test_dict.get("name", ""))
+
+        # parse test request
+        raw_request = test_dict.get('request', {})
+        parsed_test_request = self.session_context.eval_content(raw_request)
+        self.session_context.update_test_variables("request", parsed_test_request)
+
+        # prepend url with base_url unless it's already an absolute URL
+        url = parsed_test_request.pop('url')
+        base_url = self.session_context.eval_content(test_dict.get("base_url", ""))
+        parsed_url = utils.build_url(base_url, url)
+
+        # setup hooks
+        setup_hooks = test_dict.get("setup_hooks", [])
+        if setup_hooks:
+            self.do_hook_actions(setup_hooks, "setup")
+
+        try:
+            method = parsed_test_request.pop('method')
+            parsed_test_request.setdefault("verify", self.verify)
+            group_name = parsed_test_request.pop("group", None)
+        except KeyError:
+            raise exceptions.ParamsError("URL or METHOD missed!")
+
+        # TODO: move method validation to json schema
+        valid_methods = ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+        if method.upper() not in valid_methods:
+            err_msg = u"Invalid HTTP method! => {}\n".format(method)
+            err_msg += "Available HTTP methods: {}".format("/".join(valid_methods))
+            logger.log_error(err_msg)
+            raise exceptions.ParamsError(err_msg)
+
+        logger.log_info("{method} {url}".format(method=method, url=parsed_url))
+        logger.log_debug("request kwargs(raw): {kwargs}".format(kwargs=parsed_test_request))
+
+        # request
+        resp = self.http_client_session.request(
+            method,
+            parsed_url,
+            name=(group_name or test_name),
+            **parsed_test_request
+        )
+        resp_obj = response.ResponseObject(resp)
+
+        # teardown hooks
+        teardown_hooks = test_dict.get("teardown_hooks", [])
+        if teardown_hooks:
+            self.session_context.update_test_variables("response", resp_obj)
+            self.do_hook_actions(teardown_hooks, "teardown")
+
+        # extract
+        extractors = test_dict.get("extract", {})
+        extracted_variables_mapping = resp_obj.extract_response(extractors)
+        self.session_context.update_session_variables(extracted_variables_mapping)
+
+        # validate
+        validators = test_dict.get("validate") or test_dict.get("validators") or []
+        try:
+            self.session_context.validate(validators, resp_obj)
+        except (exceptions.ParamsError, exceptions.ValidationFailure, exceptions.ExtractFailure):
+            err_msg = "{} DETAILED REQUEST & RESPONSE {}\n".format("*" * 32, "*" * 32)
+
+            # log request
+            err_msg += "====== request details ======\n"
+            err_msg += "url: {}\n".format(parsed_url)
+            err_msg += "method: {}\n".format(method)
+            err_msg += "headers: {}\n".format(parsed_test_request.pop("headers", {}))
+            for k, v in parsed_test_request.items():
+                v = utils.omit_long_data(v)
+                err_msg += "{}: {}\n".format(k, repr(v))
+
+            err_msg += "\n"
+
+            # log response
+            err_msg += "====== response details ======\n"
+            err_msg += "status_code: {}\n".format(resp_obj.status_code)
+            err_msg += "headers: {}\n".format(resp_obj.headers)
+            err_msg += "body: {}\n".format(repr(resp_obj.text))
+            logger.log_error(err_msg)
+
+            raise
+
+        finally:
+            self.validation_results = self.session_context.validation_results
+
+    def _run_testcase(self, testcase_dict):
+        """ run single testcase.
+        """
+        self.meta_datas = []
+        config = testcase_dict.get("config", {})
+
+        # each teststeps in one testcase (YAML/JSON) share the same session.
+        test_runner = Runner(config, self.http_client_session)
+
+        tests = testcase_dict.get("teststeps", [])
+
+        for index, test_dict in enumerate(tests):
+
+            # override current teststep variables with former testcase output variables
+            former_output_variables = self.session_context.test_variables_mapping
+            if former_output_variables:
+                test_dict.setdefault("variables", {})
+                test_dict["variables"].update(former_output_variables)
+
+            try:
+                test_runner.run_test(test_dict)
+            except Exception:
+                # log exception request_type and name for locust stat
+                self.exception_request_type = test_runner.exception_request_type
+                self.exception_name = test_runner.exception_name
+                raise
+            finally:
+                _meta_datas = test_runner.meta_datas
+                self.meta_datas.append(_meta_datas)
+
+        self.session_context.update_session_variables(
+            test_runner.export_variables(test_runner.export)
+        )
+
+    def run_test(self, test_dict):
+        """ run single teststep of testcase.
+            test_dict may be in 3 types.
+
+        Args:
+            test_dict (dict):
+
+                # teststep
+                {
+                    "name": "teststep description",
+                    "variables": [],        # optional
+                    "request": {
+                        "url": "http://127.0.0.1:5000/api/users/1000",
+                        "method": "GET"
+                    }
+                }
+
+                # nested testcase
+                {
+                    "config": {...},
+                    "teststeps": [
+                        {...},
+                        {...}
+                    ]
+                }
+
+                # TODO: function
+                {
+                    "name": "exec function",
+                    "function": "${func()}"
+                }
+
+        """
+        self.meta_datas = None
+        if "teststeps" in test_dict:
+            # nested testcase
+            test_dict.setdefault("config", {}).setdefault("variables", {})
+            test_dict["config"]["variables"].update(
+                self.session_context.session_variables_mapping)
+            self._run_testcase(test_dict)
+        else:
+            # api
+            try:
+                self._run_test(test_dict)
+            except Exception:
+                # log exception request_type and name for locust stat
+                self.exception_request_type = test_dict["request"]["method"]
+                self.exception_name = test_dict.get("name")
+                raise
+            finally:
+                self.meta_datas = self.__get_test_data()
+
+    def export_variables(self, output_variables_list):
+        """ export current testcase variables
+        """
+        variables_mapping = self.session_context.session_variables_mapping
+
+        output = {}
+        for variable in output_variables_list:
+            if variable not in variables_mapping:
+                logger.log_warning(
+                    "variable '{}' can not be found in variables mapping, failed to export!"\
+                        .format(variable)
+                )
+                continue
+
+            output[variable] = variables_mapping[variable]
+
+        utils.print_info(output)
+        return output
